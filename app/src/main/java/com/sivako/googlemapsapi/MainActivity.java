6d2c71a7f2b5b1b2a8c953c5dcaf3b1ceb02713c
Khulu Mkhonza <khulu.mkhonza@dividebuy.co.uk>
2021-02-02 19:05:19 +0000

    override method implementation

--- app/src/main/java/com/sivako/googlemapsapi/MainActivity.java
+++ app/src/main/java/com/sivako/googlemapsapi/MainActivity.java
@@ -1,14 +1,19 @@
 package com.sivako.googlemapsapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.provider.Settings;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -16,14 +21,24 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    MapView mapView;
+    boolean isPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mapView = findViewById(R.id.map_view);
+
         checkPermissions();
+
+        if (isPermissionGranted) {
+            mapView.getMapAsync(this);
+            mapView.onCreate(savedInstanceState);
+        }
 
     }
 
@@ -34,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 
                 Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                isPermissionGranted = true;
 
             }
 
@@ -55,4 +71,59 @@ public class MainActivity extends AppCompatActivity {
         }).check();
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle bundle, @NonNull PersistableBundle persistableBundle) {
+        super.onSaveInstanceState(bundle, persistableBundle);
+        mapView.onSaveInstanceState(bundle);
+    }
+
+
 }
